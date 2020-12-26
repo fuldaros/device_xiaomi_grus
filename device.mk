4552415c15af12072879a5998f84155d1f567230
xyzx <litefros@yandex.ru>
2020-12-27 02:36:56 +0300

    grus: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead

--- device.mk
+++ device.mk
@@ -87,8 +87,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio@6.0 \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@2.0-impl \
